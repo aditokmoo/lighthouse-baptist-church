a379f1d0a0e7c3e5c6e3f7c87c916e86075e6388
Adi Tokmo <31807968+aditokmoo@users.noreply.github.com>
2024-06-07 10:10:47 +0200

    finished

--- src/pages/Contact/Form/Form.jsx
+++ src/pages/Contact/Form/Form.jsx
@@ -1,25 +1,30 @@
-import styles from './Form.module.scss';
-import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import emailjs from '@emailjs/browser';
-import { MdOutlineErrorOutline } from 'react-icons/md';
+import styles from "./Form.module.scss";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import emailjs from "@emailjs/browser";
+import { MdOutlineErrorOutline } from "react-icons/md";
 
 const SERVICE_ID = "service_ou0r9a4";
 const TEMPLATE_ID = "template_gcx5iwi";
 const USER_ID = "actHWvtGVmY6u3NRG";
 
 export default function Form() {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
 
     const sendEmail = async (data) => {
         try {
             await emailjs.send(SERVICE_ID, TEMPLATE_ID, data, USER_ID);
-            toast.success('Email sent successfully!');
-            console.log(data)
+            toast.success("Email sent successfully!");
+            console.log(data);
             reset();
         } catch (error) {
-            console.log(error)
-            toast.error('Something went wrong');
+            console.log(error);
+            toast.error("Something went wrong");
         }
     };
 
@@ -30,16 +35,48 @@ export default function Form() {
                     <form onSubmit={handleSubmit((data) => sendEmail(data))}>
                         <div className={styles.formContainer}>
                             <div className={styles.inputContainer}>
-                                <input type="text" placeholder='Name *' id='name' className={styles.input} {...register('name')}  required/>
-                                {errors.name && <p className={styles.errorMsg}>Name is required. <MdOutlineErrorOutline /></p>}
+                                <input
+                                    type="text"
+                                    placeholder="Name *"
+                                    id="name"
+                                    className={styles.input}
+                                    {...register("name")}
+                                    required
+                                />
+                                {errors.name && (
+                                    <p className={styles.errorMsg}>
+                                        Name is required. <MdOutlineErrorOutline />
+                                    </p>
+                                )}
                             </div>
                             <div className={styles.inputContainer}>
-                                <input type="email" placeholder='Email *' id='email' className={styles.input} {...register('email')}  required/>
-                                {errors.email && <p className={styles.errorMsg}>Email is required. <MdOutlineErrorOutline /></p>}
+                                <input
+                                    type="email"
+                                    placeholder="Email *"
+                                    id="email"
+                                    className={styles.input}
+                                    {...register("email")}
+                                    required
+                                />
+                                {errors.email && (
+                                    <p className={styles.errorMsg}>
+                                        Email is required. <MdOutlineErrorOutline />
+                                    </p>
+                                )}
                             </div>
                             <div className={styles.inputContainer}>
-                                <textarea placeholder='Your message *' id='message' className={styles.textarea} {...register('message')} required />
-                                {errors.message && <p className={styles.errorMsg}>Message is required. <MdOutlineErrorOutline /></p>}
+                                <textarea
+                                    placeholder="Your message *"
+                                    id="message"
+                                    className={styles.textarea}
+                                    {...register("message")}
+                                    required
+                                />
+                                {errors.message && (
+                                    <p className={styles.errorMsg}>
+                                        Message is required. <MdOutlineErrorOutline />
+                                    </p>
+                                )}
                             </div>
                         </div>
                         <button>Submit</button>
@@ -47,5 +84,5 @@ export default function Form() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
